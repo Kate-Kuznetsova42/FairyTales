b6020bfbf8a5732453ec6754ea2940d77788b685
KateFairyTale <katekuznetsova42@gmail.com>
2025-06-18 10:21:09 +0300

    удаляем лишнии комментарии и небольшие изменения

--- app/src/main/java/com/example/fairytales/SettingsActivity.java
+++ app/src/main/java/com/example/fairytales/SettingsActivity.java
@@ -16,39 +16,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class SettingsActivity extends AppCompatActivity {
-//    int size_text = 14;
-    //String color_background = "#FFFFFF";
     ConstraintLayout view;
     Button buttonSave;
     String nameClass;
-
-    //Button sizeBig2, sizeBig1, sizeMedium, sizeSmall;
-    //Button colorWh, colorSep, colorGr, colorBl;
-
-    final static String SizeTextKey = "SizeText";
-    final static String ColorBackgroundKey = "ColorBackground";
-    private static final String LOG_TAG = SettingsActivity.class.getSimpleName();
-
-
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //outState.putString(ColorBackgroundKey, color_background);
-        //outState.putInt(SizeTextKey, size_text);
-        //Log.i(LOG_TAG, "onSaveInstanceState");
-    }
-
-    // получение ранее сохраненного состояния
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        //size_text = savedInstanceState.getInt(SizeTextKey);
-        //color_background = savedInstanceState.getString(ColorBackgroundKey);
-        //view = (ConstraintLayout) findViewById(R.id.settings_layout_id);
-        //view.setBackgroundColor(Color.parseColor(color_background));
-        //Log.i(LOG_TAG, "onRestoreInstanceState");
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -61,13 +31,7 @@ public class SettingsActivity extends AppCompatActivity {
             nameClass = extras.getString("class");
         }
 
-//        Bundle arguments = getIntent().getExtras();
-//        if(arguments!=null) {
-//            //color_background = arguments.getString(ColorBackgroundKey);
-//            size_text = arguments.getInt(SizeTextKey);
-//        }
         view = (ConstraintLayout) findViewById(R.id.settings_layout_id);
-        //view.setBackgroundColor(Color.parseColor(color_background));
 
         findViewById(R.id.sizeBig2).setOnClickListener((view)->{
             ThemeManager.saveTextSize(this, ThemeManager.TEXT_SIZE_25);
@@ -100,12 +64,13 @@ public class SettingsActivity extends AppCompatActivity {
                     if (nameClass.equals(getString(R.string.name_class_add_and_change_fairy_tales_activity))) {
                         intent = new Intent(SettingsActivity.this, AddAndChangeFairyTalesActivity.class);
                         startActivity(intent);
-                    }
-                    if (nameClass.equals(getString(R.string.name_class_fairy_tales_activity))) {
+                    } else if (nameClass.equals(getString(R.string.name_class_fairy_tales_activity))) {
                         intent = new Intent(SettingsActivity.this, FairyTalesActivity.class);
                         startActivity(intent);
-                    }
-                    if (nameClass.equals(getString(R.string.name_class_main_activity))) {
+                    } else if (nameClass.equals(getString(R.string.name_class_main_activity))) {
+                        intent = new Intent(SettingsActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
                         intent = new Intent(SettingsActivity.this, MainActivity.class);
                         startActivity(intent);
                     }
@@ -115,34 +80,10 @@ public class SettingsActivity extends AppCompatActivity {
             }
         };
         buttonSave.setOnClickListener(clckLstnrSave);
-
-        //textView.setTextSize(28);
-        //textViewContextMenu.setBackgroundColor(Color.RED);
-
-        /*addBtn = (Button) findViewById(R.id.button);
-        View.OnClickListener clckLstnrOPEN = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fairyTaleAdd = new FairyTale(nameFTEditText.getText().toString());
-                writerFile(fairyTaleAdd, textFTEditText.getText().toString());
-
-                Intent intent = new Intent(AddFairyTalesActivity.this, MainActivity.class);
-                intent.putExtra(FairyTale.class.getSimpleName(), fairyTaleAdd);
-                startActivity(intent);
-            }
-        };
-        addBtn.setOnClickListener(clckLstnrOPEN);*/
-    }
-    // обработка нажатия на кнопку размера текста
-    public void onSizeClick(int size){
-//        size_text = size;
     }
     // обработка нажатия на кнопку цвета фона
     public void onColorClick(String color){
-        //color_background = color;
         ThemeManager.saveTheme(this, color);
         recreate(); // Перезапуск активности для применения темы
-//        view = (ConstraintLayout) findViewById(R.id.settings_layout_id);
-//        //view.setBackgroundColor(Color.parseColor(color_background));
     }
 }
